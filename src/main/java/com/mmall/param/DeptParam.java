87f7aedd181beae2fb971dcd715b72531b0b5eeb
linmt <549343315@qq.com>
2019-02-17 23:38:23 +0800

    190217    完成到分页插件部分

---
+++ src/main/java/com/mmall/param/DeptParam.java
@@ -0,0 +1,31 @@
+package com.mmall.param;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@ToString
+public class DeptParam {
+
+    private Integer id;
+
+    @NotBlank(message = "部门名称不可以为空")
+    @Length(max = 15, min = 2, message = "部门名称长度需要在2-15个字之间")
+    private String name;
+
+    //为了避免后续出现空指针异常，可以在这里设置默认值为0
+    private Integer parentId = 0;
+//    private Integer parentId;
+
+    @NotNull(message = "展示顺序不可以为空")
+    private Integer seq;
+
+    @Length(max = 150, message = "备注的长度需要在150个字以内")
+    private String remark;
+}
