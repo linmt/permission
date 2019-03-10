aae3d805367085f530af975d42f6e4a3ec0eb575
linmt <549343315@qq.com>
2019-03-10 23:37:51 +0800

    190310   小的改动

--- src/main/java/com/mmall/controller/TestController.java
+++ src/main/java/com/mmall/controller/TestController.java
@@ -33,7 +33,15 @@ public class TestController {
     @ResponseBody
     public JsonData hello() {
         log.info("hello");
-//        throw new PermissionException("test exception");
+        throw new PermissionException("test exception");
+//         return JsonData.success("hello, permission");
+//        throw new RuntimeException("test RuntimeException");
+    }
+
+    @RequestMapping("/hellopage.page")
+    @ResponseBody
+    public JsonData hellopage() {
+        log.info("hellopage");
 //         return JsonData.success("hello, permission");
         throw new RuntimeException("test RuntimeException");
     }
@@ -47,23 +55,32 @@ public class TestController {
     http://localhost:8080/test/validate.json?id=1&msg=hello
     2019-02-16 17:17:51.075 ["http-apr-8080"-exec-7] INFO  com.mmall.controller.TestController - validate
      */
-//    @RequestMapping("/validate.json")
-//    @ResponseBody
-//    public JsonData validate(TestVo vo) {
-//        log.info("validate");
-//        try {
-//            Map<String,String> map=BeanValidator.validateObject(vo);
-//            if(map!=null&&map.entrySet().size()>0){
-//                for(Map.Entry<String,String> entry:map.entrySet()){
-//                    log.info("{}->{}",entry.getKey(),entry.getValue());
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return JsonData.success("test validate");
-//    }
+    @RequestMapping("/validate.json")
+    @ResponseBody
+    public JsonData validate(TestVo vo) {
+        log.info("validate");
+        try {
+            Map<String,String> map=BeanValidator.validateObject(vo);
+            if(map!=null&&map.entrySet().size()>0){
+                for(Map.Entry<String,String> entry:map.entrySet()){
+                    log.info("{}->{}",entry.getKey(),entry.getValue());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return JsonData.success("test validate");
+    }
 
+    /*
+    http://localhost:8080/test/validate.json
+    {"ret":false,"msg":"{msg=不能为空, str=不能为空, id=不能为null}","data":null}
+
+    2019-03-10 22:37:19.754 [http-nio-8080-exec-13] INFO  org.hibernate.validator.internal.util.Version - HV000001: Hibernate Validator 5.2.4.Final
+    2019-03-10 22:37:21.766 [http-nio-8080-exec-13] INFO  com.mmall.common.HttpInterceptor - request start. url:/test/validate.json, params:{}
+    2019-03-10 22:37:21.832 [http-nio-8080-exec-13] INFO  com.mmall.controller.TestController - validate
+    2019-03-10 22:37:22.290 [http-nio-8080-exec-13] INFO  com.mmall.common.HttpInterceptor - request completed. url:/test/validate.json, cost:524
+     */
 //    @RequestMapping("/validate.json")
 //    @ResponseBody
 //    public JsonData validate(TestVo vo) throws ParamException {
@@ -73,14 +90,14 @@ public class TestController {
 //    }
 
     //http://localhost:8080/test/validate.json?id=1&msg=hello
-    @RequestMapping("/validate.json")
-    @ResponseBody
-    public JsonData validate(TestVo vo) throws ParamException {
-        log.info("validate");
-        SysAclModuleMapper moduleMapper = ApplicationContextHelper.popBean(SysAclModuleMapper.class);
-        SysAclModule module = moduleMapper.selectByPrimaryKey(1);
-        log.info(JsonMapper.obj2String(module));
-        BeanValidator.check(vo);
-        return JsonData.success("test validate");
-    }
+//    @RequestMapping("/validate.json")
+//    @ResponseBody
+//    public JsonData validate(TestVo vo) throws ParamException {
+//        log.info("validate");
+//        SysAclModuleMapper moduleMapper = ApplicationContextHelper.popBean(SysAclModuleMapper.class);
+//        SysAclModule module = moduleMapper.selectByPrimaryKey(1);
+//        log.info(JsonMapper.obj2String(module));
+//        BeanValidator.check(vo);
+//        return JsonData.success("test validate");
+//    }
 }
--- src/main/webapp/WEB-INF/views/exception.jsp
+++ src/main/webapp/WEB-INF/views/exception.jsp
@@ -1,5 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head></head>
-<body></body>
+<body>这是exception.jsp</body>
 </html>
