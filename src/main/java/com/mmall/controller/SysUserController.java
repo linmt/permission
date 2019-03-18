168b452ee7aac6ee3df5cfbaad45c3d995b2771c
linmt <43060283+linmt@users.noreply.github.com>
2019-03-18 17:26:32 +0800

    190318     权限拦截模块开发   完成

--- src/main/java/com/mmall/controller/SysUserController.java
+++ src/main/java/com/mmall/controller/SysUserController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import java.util.Map;
@@ -27,11 +28,6 @@ public class SysUserController {
     private SysTreeService sysTreeService;
     @Resource
     private SysRoleService sysRoleService;
-
-//    @RequestMapping("/noAuth.page")
-//    public ModelAndView noAuth() {
-//        return new ModelAndView("noAuth");
-//    }
 
     @RequestMapping("/save.json")
     @ResponseBody
@@ -62,5 +58,10 @@ public class SysUserController {
         map.put("acls", sysTreeService.userAclTree(userId));
         map.put("roles", sysRoleService.getRoleListByUserId(userId));
         return JsonData.success(map);
+    }
+
+    @RequestMapping("/noAuth.page")
+    public ModelAndView noAuth() {
+        return new ModelAndView("noAuth");
     }
 }
--- src/main/java/com/mmall/service/SysCoreService.java
+++ src/main/java/com/mmall/service/SysCoreService.java
@@ -6,11 +6,14 @@ import com.mmall.dao.SysAclMapper;
 import com.mmall.dao.SysRoleAclMapper;
 import com.mmall.dao.SysRoleUserMapper;
 import com.mmall.model.SysAcl;
+import com.mmall.model.SysUser;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class SysCoreService {
@@ -50,12 +53,12 @@ public class SysCoreService {
     public boolean isSuperAdmin() {
         // 这里是我自己定义了一个假的超级管理员规则，实际中要根据项目进行修改
         // 可以是配置文件获取，可以指定某个用户，也可以指定某个角色
-//        SysUser sysUser = RequestHolder.getCurrentUser();
-//        if (sysUser.getMail().contains("admin")) {
-//            return true;
-//        }
+        SysUser sysUser = RequestHolder.getCurrentUser();
+        if (sysUser.getMail().contains("admin")) {
+            return true;
+        }
+        return false;
 //        return false;
-        return true;
     }
 
     public List<SysAcl> getRoleAclList(int roleId) {
@@ -66,36 +69,38 @@ public class SysCoreService {
         return sysAclMapper.getByIdList(aclIdList);
     }
 
-//    public boolean hasUrlAcl(String url) {
-//        if (isSuperAdmin()) {
-//            return true;
-//        }
-//        List<SysAcl> aclList = sysAclMapper.getByUrl(url);
-//        if (CollectionUtils.isEmpty(aclList)) {
-//            return true;
-//        }
-//
+    public boolean hasUrlAcl(String url) {
+        if (isSuperAdmin()) {
+            return true;
+        }
+        List<SysAcl> aclList = sysAclMapper.getByUrl(url);
+        if (CollectionUtils.isEmpty(aclList)) {
+            return true;
+        }
+
 //        List<SysAcl> userAclList = getCurrentUserAclListFromCache();
-//        Set<Integer> userAclIdSet = userAclList.stream().map(acl -> acl.getId()).collect(Collectors.toSet());
-//
-//        boolean hasValidAcl = false;
-//        // 规则：只要有一个权限点有权限，那么我们就认为有访问权限
-//        for (SysAcl acl : aclList) {
-//            // 判断一个用户是否具有某个权限点的访问权限
-//            if (acl == null || acl.getStatus() != 1) { // 权限点无效
-//                continue;
-//            }
-//            hasValidAcl = true;
-//            if (userAclIdSet.contains(acl.getId())) {
-//                return true;
-//            }
-//        }
-//        if (!hasValidAcl) {
-//            return true;
-//        }
-//        return false;
-//    }
-//
+        List<SysAcl> userAclList = getCurrentUserAclList();
+        Set<Integer> userAclIdSet = userAclList.stream().map(acl -> acl.getId()).collect(Collectors.toSet());
+
+        boolean hasValidAcl = false;
+        // 规则：只要有一个权限点有权限，那么我们就认为有访问权限
+        for (SysAcl acl : aclList) {
+            // 判断一个用户是否具有某个权限点的访问权限
+            if (acl == null || acl.getStatus() != 1) { // 权限点无效
+                continue;
+            }
+            hasValidAcl = true;
+            if (userAclIdSet.contains(acl.getId())) {
+                return true;
+            }
+        }
+        //如果当前没有可以校验的权限点的时候，直接返回true
+        if (!hasValidAcl) {
+            return true;
+        }
+        return false;
+    }
+
 //    public List<SysAcl> getCurrentUserAclListFromCache() {
 //        int userId = RequestHolder.getCurrentUser().getId();
 //        String cacheValue = sysCacheService.getFromCache(CacheKeyConstants.USER_ACLS, String.valueOf(userId));
